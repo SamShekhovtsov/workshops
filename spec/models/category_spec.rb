496d84c524e3de14a16e258bee048d123bb98d59
Sem Shekhovtsov <semshehovtsov@gmail.com>
2015-10-14 01:23:18 +0800

    fixing models validation

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Category do
-  #it { should validate_uniqueness_of(:name) }
+  describe 'validations' do
+	it { should validate_presence_of :name }
+  	it { should validate_uniqueness_of(:name) }
+  end
 end

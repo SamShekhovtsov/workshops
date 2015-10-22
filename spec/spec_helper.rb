fb334ba445e7fe8f14222c16ca52814c33fb86fd
Sem Shekhovtsov <semshehovtsov@gmail.com>
2015-10-22 12:20:51 +0800

    fixed spec warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
+RSpec.configure do |config|
+  config.raise_errors_for_deprecations!
+end
+
 ENV["RAILS_ENV"] ||= 'test'
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+#require 'rspec/autorun'
 require 'database_cleaner'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -10,7 +15,7 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
   config.include FactoryGirl::Syntax::Methods
   config.infer_base_class_for_anonymous_controllers = false
-  config.raise_errors_for_deprecations!
+  #config.raise_errors_for_deprecations!
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
@@ -22,4 +27,12 @@ RSpec.configure do |config|
 
   config.order = "random"
   config.infer_spec_type_from_file_location!
+
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 end

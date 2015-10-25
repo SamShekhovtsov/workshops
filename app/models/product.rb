f46c7ba1e3d311010f5d4a2ebcff13adfb3593e8
Sem Shekhovtsov <semshehovtsov@gmail.com>
2015-10-25 18:50:19 +0800

    fixing product model spec

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ActiveRecord::Base
 
   validates :title, :description, :user_id, presence:true
   validates :price, presence:true, numericality:true, format: { with: /\A\d{1,4}(\.\d{0,2})?\z/ }
+
+  def average_rating
+  	self.reviews.inject{|sum,x| sum.rating + x.rating } / self.reviews.count.to_f
+  end
 end
